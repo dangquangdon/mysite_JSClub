abc94890866de78487572eef7b5189fc0e479f7f
dangquangdon <dangquangdon@gmail.com>
2019-03-31 00:07:43 +0200

    routes to add work exp and education information, start working on get user by handle

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
 /*
  * Landing page
  * root
@@ -22,6 +23,24 @@ router.get('/about', (req, res) => {
 });
 
 /*
+ * About page of a user
+ * /:handle/about
+ * Public
+ * Get
+ */
+router.get('/:handle/about', (req, res) => {
+  User.findOne({ handle: req.params.handle })
+    .then(user => {
+      if (user) {
+        res.render('about', { handle_user: user });
+      } else {
+        req.flash('error_message', 'This user does not exist');
+        res.redirect('/about');
+      }
+    })
+    .catch(err => console.log(err));
+});
+/*
  * Contact page
  * /contact
  * Public
@@ -32,7 +51,8 @@ router.get('/contact', (req, res) => {
 });
 
 /*
- * export router
- * always at the bottom
+      TO DO:
+        * Show user's information by hanle
+        * Home, Portfolio, Contact
  */
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -63,4 +63,88 @@ router.put('/update-profile', isAuthorized, (req, res) => {
     .catch(err => console.log(err));
 });
 
+/*
+ * Add education
+ * /user/update-education
+ * Private
+ * GET
+ */
+
+router.get('/update-education', isAuthorized, (req, res) => {
+  User.findById(req.user._id)
+    .then(user => {
+      res.render('update_education', { id: user._id });
+    })
+    .catch(err => console.log(err));
+});
+
+// POST - Add/Update education form
+router.post('/update-education', isAuthorized, (req, res) => {
+  User.findById(req.user._id)
+    .then(user => {
+      const school = {
+        school: req.body.school,
+        degree: req.body.degree,
+        year: req.body.year
+      };
+
+      user.education.unshift(school);
+      user.education_description = req.body.education_description;
+
+      user
+        .save()
+        .then(user => {
+          req.flash('success_message', 'Add education infomation successfully');
+          res.redirect('/');
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+
+/*
+ * Add workexp
+ * /user/update-workexp
+ * Private
+ * GET
+ */
+
+router.get('/update-workexp', isAuthorized, (req, res) => {
+  User.findById(req.user._id)
+    .then(user => {
+      res.render('update_workexp', { id: user._id });
+    })
+    .catch(err => console.log(err));
+});
+
+// POST - Add/Update workexp form
+router.post('/update-workexp', isAuthorized, (req, res) => {
+  User.findById(req.user._id)
+    .then(user => {
+      const company = {
+        company: req.body.company,
+        title: req.body.title,
+        year: req.body.year
+      };
+
+      user.workexp.unshift(company);
+      user
+        .save()
+        .then(user => {
+          req.flash(
+            'success_message',
+            'Add work experience infomation successfully'
+          );
+          res.redirect('/');
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+
+/*
+    TO DO:
+            * Add and Delete Education by id
+            * Add and Delete Work EXP by id
+ */
 module.exports = router;
