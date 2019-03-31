bf27203b5e736b69ca057bc8cbec2ea514274523
dangquangdon <dangquangdon@gmail.com>
2019-03-31 23:48:27 +0300

    deploy heroku

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,19 @@ module.exports = {
   mongoURI: 'mongodb://don:don1234@ds038319.mlab.com:38319/mysite2019',
   secretKey: 'topsecret'
 };
+
+/**
+    If you are using mongoDB locally, you might need to use this configuration
+ */
+
+// if (process.env.NODE_ENV === 'production'){
+//   module.exports = {
+//     mongoURI: 'mlab URI',
+//     secretKey: 'topsecret'
+//   };
+// }else{
+//   module.exports = {
+//     mongoURI: 'local URI',
+//     secretKey: 'topsecret'
+//   };
+// }
